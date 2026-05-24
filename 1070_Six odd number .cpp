2a8f81b53e356b4ebb845e4cc6b29d196606aad0
yasmin-coder-1510 <yasminakter20031@gmail.com>
2026-05-24 13:08:37 +0600

    Create 1070_Six odd number .cpp

---
+++ 1070_Six odd number .cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int x;
+ cin>>x;
+
+     if(x%2==0)
+     {
+       x++;  
+     }
+    for(int i=1;i<=6;i++)
+    {
+        cout<<x<<endl;
+        x+=2;
+    }
+    
+ return 0;   
+}
